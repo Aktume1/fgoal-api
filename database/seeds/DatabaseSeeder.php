ea41a234b270977657fb225c4bf95dc3f4b17f42
thanhdolla <thanhdolla198@gmail.com>
2018-09-07 11:43:29 +0700

    fix get obj by quater

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -5,6 +5,7 @@ use App\Eloquent\Group;
 use App\Eloquent\User;
 use App\Eloquent\Quarter;
 use App\Eloquent\Unit;
+use App\Eloquent\Objective;
 
 class DatabaseSeeder extends Seeder
 {
@@ -19,6 +20,7 @@ class DatabaseSeeder extends Seeder
         Group::query()->truncate();
         Quarter::query()->truncate();
         Unit::query()->truncate();
+        Objective::query()->truncate();
 
         $this->call(UsersTableSeeder::class);
         $this->call(WorkspacesTableSeeder::class);
