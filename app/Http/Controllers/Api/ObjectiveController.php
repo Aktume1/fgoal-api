2ebeb6fddb0435023b9277e44bad03e6074f2e9f
nguyenngocsond <40288953+nguyenngocsond@users.noreply.github.com>
2018-09-12 11:17:00 +0700

    Revert "manager verify link objective"

--- app/Http/Controllers/Api/ObjectiveController.php
+++ app/Http/Controllers/Api/ObjectiveController.php
@@ -171,17 +171,6 @@ class ObjectiveController extends ApiController
         });
     }
 
-    /**
-     * Add link on objective
-     *
-     * @param $groupId
-     * @param $objectiveId
-     * @return \Illuminate\Http\JsonResponse
-     * @throws \App\Exceptions\Api\ActionException
-     * @throws \App\Exceptions\Api\NotFoundException
-     * @throws \App\Exceptions\Api\NotOwnerException
-     * @throws \App\Exceptions\Api\UnknownException
-     */
     public function removeLinkObjective($groupId, $objectiveId)
     {
         return $this->doAction(function () use ($groupId, $objectiveId) {
@@ -214,22 +203,6 @@ class ObjectiveController extends ApiController
     {
         return $this->getData(function () use ($objectiveId) {
             $this->compacts['data'] = $this->objectiveRepository->getObjectiveLogById($objectiveId);
-        });
-    }
-
-    /**
-     * Admin verify link objective
-     *
-     * @param $groupId
-     * @param $objectiveId
-     * @return \Illuminate\Http\JsonResponse
-     * @throws \App\Exceptions\Api\UnknownException
-     */
-    public function verifyLinkObjective($groupId, $objectiveId)
-    {
-        return $this->doAction(function () use ($groupId, $objectiveId) {
-            $this->compacts['data'] = $this->objectiveRepository->verifyLink($groupId, $objectiveId);
-            $this->compacts['description'] = translate('success.verify_link');
         });
     }
 }
