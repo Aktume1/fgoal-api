16b8c02b6f72b90feb90d78f76b6b2610b99bfaf
thanhpv958 <thanhpv958@gmail.com>
2018-10-10 15:06:58 +0700

    fix match value

---
+++ database/migrations/2018_10_10_074831_update_08_objectives_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Update08ObjectivesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('objectives', function (Blueprint $table) {
+            $table->tinyInteger('match')->default(1)->after('status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('objectives', function (Blueprint $table) {
+            $table->dropColumn('match');
+        });
+    }
+}
